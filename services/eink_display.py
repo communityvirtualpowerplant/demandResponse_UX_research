c93265a9195be8f0d95471a46f2adde661cea833
alexnathanson <alex@alexnathanson.com>
2025-08-05 20:23:01 -0400

    shift left

--- services/eink_display.py
+++ services/eink_display.py
@@ -317,7 +317,7 @@ def eventScreen(f,s, p):
 
 
     # time
-    centerX = 2*screenWidth/3  - (screenWidth/6)
+    centerX = 2*screenWidth/3  - (screenWidth/6) - 5
     sDraw.circle((centerX,centerY),circRad,fill=255, outline=0,width=1)
     sDraw.pieslice((centerX-circRad,centerY-circRad,centerX+circRad,centerY+circRad), 0-90, int(360*percT)-90,fill=0)
     sDraw.circle((centerX,centerY),circRad*.5,fill=255, outline=0,width=1)
@@ -325,7 +325,7 @@ def eventScreen(f,s, p):
     sDraw.text((centerX,centerY+circRad+2), f"Your Goal", font = f,  anchor="ma",fill = 0)
 
     # performance
-    centerX = 3*screenWidth/3 - (screenWidth/6) + 3
+    centerX = 3*screenWidth/3 - (screenWidth/6) -2
     sDraw.circle((centerX,centerY),circRad,fill=255, outline=0,width=1)
     sDraw.pieslice((centerX-circRad,centerY-circRad,centerX+circRad,centerY+circRad), -90, int(360*perc)-90,fill=0)
     sDraw.circle((centerX,centerY),circRad*.5,fill=255, outline=0,width=1)
